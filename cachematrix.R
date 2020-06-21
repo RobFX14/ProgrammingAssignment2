c71dbac5227d0e415ca30d40c3dd50bd3a5ccaf5
RobFX14 <rrojas.f10@gmail.com>
2020-06-20 23:17:40 -0400

    getting ready to push an example cachematrix file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,26 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix<-function(x=matrix()){
+  m<-NULL # cached data NULL restart
+  set<-function(y){ # se puede reasignar la matrix cambiando por otra matrix
+    x<<-y
+    m<-NULL
+  }
+  get<-function() x #guarda matrix x con funcion get
+  setinverse<-function(solve) m<<-solve # guarda funcion que calcula inversa matriz
+  getinverse<-function() m # devuelve la matriz inversa
+  list(set=set,get=get,
+       setinverse=setinverse,
+       getinverse=getinverse) #lista de funciones utilizadas dentro de makeCacheMatrix
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve<-function(x,...){
+  m<-x$getinverse() 
+  if(!is.null(m)){ # si el dato m=NULL, devuelve mensaje cached
+    message("getting cached data")
+    return(m)
+  }
+  data<-x$get() # obtiene matrix x y asigna a data
+  m<-solve(data,...) # calcula inversa de x, desde m asignado por setinverse
+  x$setinverse(m)
+  m # retorna resultado inversa
 }
+# Language : ESP
